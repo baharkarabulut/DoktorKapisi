bcac2523f006751fd84f4d68920ee85719543ac9
BAHARS <BAHARS@DESKTOP-NJP4QFR>
2023-11-02 18:09:37 +0300

    Add project files.

---
+++ DoktorKapisi/Controllers/DefaultController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DoktorKapisi.Models.Entity;
+
+namespace DoktorKapisi.Controllers
+{
+    public class DefaultController : Controller
+    {
+        DoktorKapisiEntities db = new DoktorKapisiEntities();
+        // GET: Default
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Hakkimizda()
+        {
+            return View();
+        }
+
+        public ActionResult RandevuAl()
+        {
+            return View();
+        }
+
+        public ActionResult DoktorMusunuz()
+        {
+            return View();
+        }
+        public ActionResult Giris()
+        {
+            return View();
+        }
+    }
+}
